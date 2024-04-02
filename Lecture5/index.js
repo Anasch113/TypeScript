5ae3fbecde95b3f4436e31de8987600a27c2add5
Anasch113 <anaschaudry2002@gmail.com>
2024-04-02 22:21:17 +0500

    commit 4/2/24

---
+++ Lecture5/index.js
@@ -0,0 +1,6 @@
+//Boolean Types In TypeScript
+var isFull = true;
+function isDivisibleBy4and8(a) {
+    return a % 4 === 0 && a % 8 === 0;
+}
+console.log(isDivisibleBy4and8(4));
---
+++ Lecture5/index.ts
@@ -0,0 +1,12 @@
+
+
+
+//Boolean Types In TypeScript
+
+let isFull:boolean = true
+
+function isDivisibleBy4and8(a:number):boolean{
+return a %  4 ===0 && a % 8 === 0
+}
+
+console.log(isDivisibleBy4and8(4))
