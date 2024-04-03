17f1e40a2bceba1fb6caeb392c7986bf2b3c7225
Anasch113 <anaschaudry2002@gmail.com>
2024-04-03 15:38:24 +0500

    commit 3 April 24

--- Lecture5/index.js
+++ Lecture5/index.js
@@ -4,3 +4,12 @@ function isDivisibleBy4and8(a) {
     return a % 4 === 0 && a % 8 === 0;
 }
 console.log(isDivisibleBy4and8(4));
+// BigInt Types In TypeScript
+// let maxSafeNumber = Number.MAX_SAFE_INTEGER;
+// console.log(maxSafeNumber)
+var maxNumber = 9007199254740991n;
+console.log(maxNumber);
+var anotherBigNumber = BigInt("900719925474099220");
+console.log(anotherBigNumber);
+var sumOfBigNumbers = maxNumber + anotherBigNumber;
+console.log(sumOfBigNumbers);
--- Lecture5/index.ts
+++ Lecture5/index.ts
@@ -10,3 +10,23 @@ return a %  4 ===0 && a % 8 === 0
 }
 
 console.log(isDivisibleBy4and8(4))
+
+
+
+// BigInt Types In TypeScript
+
+// let maxSafeNumber = Number.MAX_SAFE_INTEGER;
+// console.log(maxSafeNumber)
+
+let maxNumber = 9007199254740991n;
+
+console.log(maxNumber)
+
+let anotherBigNumber = BigInt("900719925474099220")
+
+console.log(anotherBigNumber)
+
+
+let sumOfBigNumbers = maxNumber + anotherBigNumber
+
+console.log(sumOfBigNumbers)
