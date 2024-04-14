299a6506e4f64bf53ee314d904c3fe0c695bf211
Anasch113 <anaschaudry2002@gmail.com>
2024-04-14 23:27:16 +0500

    commit april 14 2024

---
+++ Lecture8/index.js
@@ -0,0 +1,7 @@
+// TypeInterference : It is property of typescript refers to automatically detection of type of the variable.
+var message = "Hello TypeScirpt!";
+// message = 12
+function calculateArea(length, width) {
+    return length * width;
+}
+console.log(calculateArea(2, 5));
---
+++ Lecture8/index.ts
@@ -0,0 +1,15 @@
+// TypeInterference : It is property of typescript refers to automatically detection of type of the variable.
+
+
+let message = "Hello TypeScirpt!"
+
+// message = 12
+
+
+function calculateArea(length:number, width:number){
+
+
+    return length * width
+}
+
+console.log(calculateArea(2,5))
