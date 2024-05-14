84d6a9b423047864547b6137841fa49f9cf407a4
Anasch113 <anaschaudry2002@gmail.com>
2024-05-14 23:46:57 +0500

    commit may 14 2024

---
+++ Lecture10/index.js
@@ -0,0 +1,8 @@
+// Arrays
+// Method 1
+var names = ["Anas", "Ahmad", "Ali"];
+// Method 2: Array Constructor
+var number = new Array(1, 2, 3, 4, 3, 4, 3, 44, 3434, 344, 34);
+// Method 3: Array.of
+var names2 = Array.of("anas", "ahmad");
+console.log(names2[0]);
---
+++ Lecture10/index.ts
@@ -0,0 +1,20 @@
+// Arrays
+
+
+// Method 1
+
+const names: string[] = ["Anas", "Ahmad", "Ali"]
+
+
+// Method 2: Array Constructor
+const number: number[] = new Array (1, 2, 3, 4, 3,4,3,44,3434,344,34)
+
+
+// Method 3: Array.of
+
+var names2: string[] = Array.of("anas", "ahmad") 
+
+
+console.log(names2[0])
+
+
