346b3edde390d1dd858958ce1f5292634efc67e8
Anasch113 <anaschaudry2002@gmail.com>
2024-05-26 14:41:27 +0500

    commit

---
+++ Lecture12/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Map and filter method in typescript
+const numbers = [12, 5, 12, 89, 89, 23];
+const doubleNumber = numbers.map((value) => value * 2);
+console.log(doubleNumber);
---
+++ Lecture12/index.ts
@@ -0,0 +1,12 @@
+// Map and filter method in typescript
+
+
+const numbers:number[] = [12, 5, 12, 89, 89, 23]
+
+
+const doubleNumber: number[] = numbers.map((value:number)=> value * 2)
+
+console.log(doubleNumber)
+
+
+
