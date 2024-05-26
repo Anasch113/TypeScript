af4bb89bf1dabf1303d4ff5293c8fac39e971a07
Anasch113 <anaschaudry2002@gmail.com>
2024-05-26 15:05:56 +0500

    commti

--- Lecture12/index.js
+++ Lecture12/index.js
@@ -6,3 +6,15 @@ console.log(doubleNumber);
 // Filter Method
 const evenNumbers = numbers.filter((value) => value % 2 === 0);
 console.log(evenNumbers);
+// Challenge
+// Function to find the max value from the array
+const findMaxValue = (numbers) => {
+    let maximumValue = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (maximumValue < numbers[i]) {
+            maximumValue = numbers[i];
+        }
+    }
+    return maximumValue;
+};
+console.log(findMaxValue([4, 2, 42, 2, 3]));
--- Lecture12/index.ts
+++ Lecture12/index.ts
@@ -13,10 +13,61 @@ console.log(doubleNumber)
 // Filter Method
 
 
-const evenNumbers = numbers.filter((value) => value % 2 === 0)
+const evenNumbers: number[] = numbers.filter((value:number) => value % 2 === 0)
 
 console.log(evenNumbers)
 
+
+// Challenge
+
+// Function to find the max value from the array
+
+const findMaxValue = (numbers:any):number =>{
+    let maximumValue:number = 0
+    for(let i:number=0; i < numbers.length; i++){
+        if (maximumValue < numbers[i]){
+            maximumValue = numbers[i];
+        }
+    }
+    return maximumValue
+
+}
+
+console.log(findMaxValue([4,2,42,2,3]));
+
+
+function findMaxUsingFilter(arr: number[]): number | undefined {
+    if (arr.length === 0) {
+      return undefined;
+    }
+  
+    let max = arr[0];
+    arr.filter(value => {
+      if (value > max) {
+        max = value;
+      }
+    });
+    return max;
+  }
+  
+  function findAverage(arr: number[]): number | undefined {
+    if (arr.length === 0) {
+      return undefined;
+    }
+  
+    const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum / arr.length;
+  }
+  
+  // Example usage
+  const numberss = [10, 20, 30, 40, 50];
+  
+  const max = findMaxUsingFilter(numberss);
+  const averagee = findAverage(numberss);
+  
+  console.log(`Maximum Value: ${max}`); // Output: Maximum Value: 50
+  console.log(`Average Value: ${averagee}`); // Output: Average Value: 30
+  
 
 
 
