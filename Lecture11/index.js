a89e24ce9baf8cd846993975bf3ce5f72408c538
Anasch113 <anaschaudry2002@gmail.com>
2024-05-26 13:59:25 +0500

    commit

---
+++ Lecture11/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Add, remove from the array. Itterations methods
+// Push
+const cars = ["BMW", "Mercedes", "Toyota", "Honda"];
+console.log(cars);
+const latestCars = cars.push("Ferrari");
+console.log(latestCars);
+console.log(cars);
+// Pop
+const newCarsData = cars.pop();
+console.log(newCarsData);
+console.log(cars);
+// Iteration through for loop
---
+++ Lecture11/index.ts
@@ -0,0 +1,31 @@
+// Add, remove from the array. Itterations methods
+
+
+// Push
+const cars:string[] = ["BMW", "Mercedes", "Toyota", "Honda"];
+console.log(cars)
+
+const latestCars = cars.push("Ferrari")
+
+console.log(latestCars)
+
+console.log(cars)
+
+
+
+// Pop
+
+
+const newCarsData = cars.pop() 
+
+console.log(newCarsData)
+console.log(cars)
+
+
+
+// Iteration through for loop
+
+
+
+
+
