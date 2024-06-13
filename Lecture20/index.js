cdf92af13023d675c84b81b60bedfb231c5799dc
Anasch113 <anaschaudry2002@gmail.com>
2024-06-13 11:11:55 +0500

    generics practice

---
+++ Lecture20/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Usage of generics and practice with examples
+// function add(a: number, b: number): number
+// function add(a: string, b: string): string
+// function add(a: any, b: any): any {
+//     return a + b
+// }
+// Converting above overloading functions into single function using generics
+function add(a, b, c) {
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+const result = add(1, "hello", true);
+const resultTwo = add("anas", "rafiq", false);
---
+++ Lecture20/index.ts
@@ -0,0 +1,25 @@
+// Usage of generics and practice with examples
+
+
+
+// function add(a: number, b: number): number
+// function add(a: string, b: string): string
+// function add(a: any, b: any): any {
+//     return a + b
+// }
+
+
+// Converting above overloading functions into single function using generics
+
+
+function add<T, U, V>(a: T, b: U , c:boolean) {
+
+    console.log(typeof a)
+    console.log(typeof b)
+    console.log(typeof c)
+
+}
+
+const result = add<number, string, boolean>(1, "hello", true)
+
+const resultTwo = add<string, string, boolean>("anas", "rafiq", false)
